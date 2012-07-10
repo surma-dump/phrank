6a1ccaee081913f9f82e12dcce4658c51eaebfd5
Alexander Surma <surma@surmair.de>
2012-07-10 22:15:32 +0200

    Implement configration (re)loading

--- gaasd.go
+++ gaasd.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"code.google.com/p/gorilla/mux"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
 	"strings"
+	"syscall"
 )
 
 var (
 	httpPort     = flag.Int("http", 80, "Port to bind HTTP server to")
 	httpsPort    = flag.Int("https", 443, "Port to bind HTTP server to")
 	domainSuffix = flag.String("domain", "", "Domain-suffix for apps")
+	configDir    = flag.String("config", "gaas.d", "Location of the config directory")
 	help         = flag.Bool("h", false, "Show this help")
 )
 
@@ -25,6 +31,15 @@ func main() {
 		flag.PrintDefaults()
 		return
 	}
+
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGUSR1)
+		for _ = range c {
+			readConfig()
+		}
+	}()
+	readConfig()
 
 	httpAddr := fmt.Sprintf("0.0.0.0:%d", *httpPort)
 	httpsAddr := fmt.Sprintf("0.0.0.0:%d", *httpsPort)
@@ -51,15 +66,48 @@ func main() {
 }
 
 var (
-	backends = map[string]*net.TCPAddr{}
+	backends = map[string]Backend{}
 )
 
-func init() {
-	var e error
-	backends["testapp"], e = net.ResolveTCPAddr("tcp4", "localhost:9001")
-	if e != nil {
-		log.Fatalf("Invalid app address: %s", e)
-	}
+type Backend struct {
+	Name             string
+	AddForwardHeader bool
+	Address          string
+}
+
+func readConfig() {
+	log.Printf("Reading configuration files...")
+	newBackends := map[string]Backend{}
+	filepath.Walk(*configDir, func(path string, info os.FileInfo, e error) error {
+		if e != nil {
+			log.Printf("Error: %s: %s", path, e)
+			return nil
+		}
+		if info.IsDir() && path != *configDir {
+			return filepath.SkipDir
+		}
+		if !strings.HasSuffix(path, ".conf") {
+			return nil
+		}
+		log.Printf("Reading %s...", path)
+		f, e := os.Open(path)
+		if e != nil {
+			log.Printf("Could not open file %s: %s", path, e)
+			return nil
+		}
+		defer f.Close()
+
+		var b Backend
+		dec := json.NewDecoder(f)
+		if e := dec.Decode(&b); e != nil {
+			log.Printf("Could not parse file %s: %s", path, e)
+			return nil
+		}
+		newBackends[strings.ToLower(b.Name)] = b
+		return nil
+	})
+	backends = newBackends
+	log.Printf("Done.")
 }
 
 func appHandler(w http.ResponseWriter, r *http.Request) {
@@ -67,13 +115,20 @@ func appHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid domain", 404)
 		return
 	}
-	// Strip domain suffix + dot
+	// Strip domain suffix + leading dot
 	appname := r.Host[0 : len(r.Host)-len(*domainSuffix)-1]
 	backend, ok := backends[appname]
 	if !ok {
 		http.Error(w, "Unknown app name", 404)
 		return
 	}
+
+	backendaddr, e := net.ResolveTCPAddr("tcp4", backend.Address)
+	if e != nil {
+		http.Error(w, "Could not resolve backend address", 500)
+		return
+	}
+
 	ccon, _, e := w.(http.Hijacker).Hijack()
 	if e != nil {
 		http.Error(w, "Hijacking failed", 500)
@@ -81,18 +136,26 @@ func appHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer ccon.Close()
 
-	bcon, e := net.DialTCP("tcp4", nil, backend)
+	bcon, e := net.DialTCP("tcp4", nil, backendaddr)
 	if e != nil {
 		log.Printf("Backend not reachable: %s", e)
 		return
 	}
 	defer bcon.Close()
 
-	r.Header.Add("X-Forwarded-For", r.RemoteAddr)
-	log.Printf("Forwarding request from %s to %s", r.RemoteAddr, backend)
+	// Add X-Forwared-For header and send request
+	if backend.AddForwardHeader {
+		r.Header.Add("X-Forwarded-For", r.RemoteAddr)
+	}
 	r.Write(bcon)
 
-	log.Printf("Waiting for answer...")
-	io.Copy(ccon, bcon)
-	log.Printf("Done")
+	// Avoid floating keep-alive connection by letting the http package
+	// parse the response. It closes the connection once everything has been
+	// received.
+	resp, e := http.ReadResponse(bufio.NewReader(bcon), r)
+	if e != nil {
+		log.Printf("Invalid response: %s", e)
+		return
+	}
+	resp.Write(ccon)
 }
