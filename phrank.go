d87f57cc8387fed97531c7e04798300d15cb70c7
Alexander Surma <surma@surmair.de>
2013-03-21 21:12:44 +0100

    Rewrite for reverse proxy

--- phrank.go
+++ phrank.go
@@ -1,144 +1,72 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
-	"flag"
+	"fmt"
 	"log"
-	"net"
 	"net/http"
+	"net/url"
 	"os"
-	"os/signal"
-	"path/filepath"
 	"strings"
-	"syscall"
+
+	"github.com/voxelbrain/goptions"
+	"github.com/voxelbrain/kartoffelsack"
 )
 
 var (
-	httpAddr  = flag.String("http", ":80", "Address to bind HTTP server to")
-	httpsAddr = flag.String("https", ":443", "Address to bind HTTP server to")
-	configDir = flag.String("config", "phrank.d", "Location of the config directory")
-	help      = flag.Bool("h", false, "Show this help")
+	options = struct {
+		Listen string        `goptions:"-l, --listen, description='Address to bind webserver to'"`
+		Maps   []*Map        `goptions:"-m, --map, description='Map a path to a ressource', obligatory"`
+		Help   goptions.Help `goptions:"-h, --help, description='Show this help'"`
+	}{
+		Listen: fmt.Sprintf(":%s", DefaultEnv("PORT", "8080")),
+	}
 )
+
+func DefaultEnv(key, def string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return def
+}
 
 func main() {
-	flag.Parse()
+	goptions.ParseAndFail(&options)
 
-	if *help {
-		flag.PrintDefaults()
-		return
+	for _, m := range options.Maps {
+		var h http.Handler
+		switch m.Ressource.Scheme {
+		case "http", "https":
+			h = kartoffelsack.NewSingleHostReverseProxy(m.Ressource)
+		default:
+			log.Fatalf("Unknown scheme: %s", m.Ressource.Scheme)
+		}
+		http.Handle(m.Path, http.StripPrefix(m.Path, h))
 	}
-
-	go func() {
-		c := make(chan os.Signal)
-		signal.Notify(c, syscall.SIGUSR1)
-		for _ = range c {
-			readConfig()
-		}
-	}()
-	readConfig()
-
-	log.Printf("Binding HTTPS to %s", *httpsAddr)
-	go func() {
-		e := http.ListenAndServeTLS(*httpsAddr, *configDir+"/cert.pem", *configDir+"/key.pem", http.HandlerFunc(appHandler))
-		if e != nil {
-			log.Printf("Could not bind HTTPS server: %s", e)
-		}
-	}()
-
-	log.Printf("Binding HTTP to %s", *httpAddr)
-	e := http.ListenAndServe(*httpAddr, http.HandlerFunc(appHandler))
-	if e != nil {
-		log.Fatalf("Could not bind HTTP server: %s", e)
+	log.Printf("Starting webserver on %s...", options.Listen)
+	err := http.ListenAndServe(options.Listen, nil)
+	if err != nil {
+		log.Fatalf("Could not start webserver: %s", err)
 	}
 }
 
-var (
-	backends = map[string]Backend{}
-)
-
-type Backend struct {
-	Domain           string
-	AddForwardHeader bool
-	Address          string
+type Map struct {
+	Path      string
+	Ressource *url.URL
 }
 
-func readConfig() {
-	log.Printf("Reading configuration files...")
-	newBackends := map[string]Backend{}
-	root := *configDir + "/apps"
-	filepath.Walk(root, func(path string, info os.FileInfo, e error) error {
-		if e != nil {
-			log.Printf("Error: %s: %s", path, e)
-			return nil
-		}
-		if info.IsDir() && path != root {
-			return filepath.SkipDir
-		}
-		if !strings.HasSuffix(path, ".app") {
-			return nil
-		}
-		log.Printf("Reading %s...", path)
-		f, e := os.Open(path)
-		if e != nil {
-			log.Printf("Could not open file %s: %s", path, e)
-			return nil
-		}
-		defer f.Close()
-
-		var b Backend
-		dec := json.NewDecoder(f)
-		if e := dec.Decode(&b); e != nil {
-			log.Printf("Could not parse file %s: %s", path, e)
-			return nil
-		}
-		newBackends[strings.ToLower(b.Domain)] = b
-		return nil
-	})
-	backends = newBackends
-	log.Printf("Done.")
-}
-
-func appHandler(w http.ResponseWriter, r *http.Request) {
-	backend, ok := backends[strings.ToLower(r.Host)]
-	if !ok {
-		http.Error(w, "Invalid domain", 404)
-		return
+func (m *Map) MarshalGoption(v string) error {
+	maps := strings.Split(v, "=>")
+	if len(maps) != 2 {
+		return fmt.Errorf("Invalid mapping")
 	}
 
-	backendaddr, e := net.ResolveTCPAddr("tcp4", backend.Address)
-	if e != nil {
-		http.Error(w, "Could not resolve backend address", 500)
-		return
+	path := strings.TrimSpace(maps[0])
+	rsrc, err := url.Parse(strings.TrimSpace(maps[1]))
+	if err != nil {
+		return fmt.Errorf("Invalid URI: %s", err)
 	}
 
-	ccon, _, e := w.(http.Hijacker).Hijack()
-	if e != nil {
-		http.Error(w, "Hijacking failed", 500)
-		return
-	}
-	defer ccon.Close()
-
-	bcon, e := net.DialTCP("tcp4", nil, backendaddr)
-	if e != nil {
-		log.Printf("Backend not reachable: %s", e)
-		return
-	}
-	defer bcon.Close()
-
-	// Add X-Forwared-For header and send request
-	if backend.AddForwardHeader {
-		r.Header.Add("X-Forwarded-For", r.RemoteAddr)
-	}
-	r.Write(bcon)
-
-	// Avoid floating keep-alive connection by letting the http package
-	// parse the response. It closes the connection once everything has been
-	// received.
-	resp, e := http.ReadResponse(bufio.NewReader(bcon), r)
-	if e != nil {
-		log.Printf("Invalid response: %s", e)
-		return
-	}
-	resp.Write(ccon)
+	m.Path = path
+	m.Ressource = rsrc
+	return nil
 }
