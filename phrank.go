1e475795f89e1df2288b04d2bfe2ea4f352fa4af
Alexander Surma <surma@surmair.de>
2013-03-21 22:02:43 +0100

    Add support for S3

--- phrank.go
+++ phrank.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
+
+	"launchpad.net/goamz/aws"
+	"launchpad.net/goamz/s3"
 
 	"github.com/voxelbrain/goptions"
 	"github.com/voxelbrain/kartoffelsack"
@@ -33,12 +37,28 @@ func main() {
 	goptions.ParseAndFail(&options)
 
 	for _, m := range options.Maps {
+		if !strings.HasSuffix(m.Path, "/") {
+			m.Path += "/"
+		}
 		var h http.Handler
 		switch m.Ressource.Scheme {
 		case "http", "https":
 			h = kartoffelsack.NewSingleHostReverseProxy(m.Ressource)
 		case "file":
 			h = http.FileServer(http.Dir(m.Ressource.Path))
+		case "s3":
+			username := m.Ressource.User.Username()
+			password, _ := m.Ressource.User.Password()
+			region, err := regionByEndpoint(m.Ressource.Host)
+			if err != nil {
+				log.Fatalf("Could not connect to S3: %s", err)
+			}
+			auth := aws.Auth{
+				AccessKey: username,
+				SecretKey: password,
+			}
+			s3acc := s3.New(auth, region)
+			h = &S3HTTP{s3acc.Bucket(m.Ressource.Path)}
 		default:
 			log.Fatalf("Unknown scheme: %s", m.Ressource.Scheme)
 		}
@@ -49,6 +69,16 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not start webserver: %s", err)
 	}
+}
+
+func regionByEndpoint(endpoint string) (aws.Region, error) {
+	for _, region := range aws.Regions {
+		epURL, _ := url.Parse(region.S3Endpoint)
+		if epURL.Host == endpoint {
+			return region, nil
+		}
+	}
+	return aws.Region{}, fmt.Errorf("No region with endpoint %s", endpoint)
 }
 
 type Map struct {
@@ -71,4 +101,19 @@ func (m *Map) MarshalGoption(v string) error {
 	m.Path = path
 	m.Ressource = rsrc
 	return nil
+}
+
+type S3HTTP struct {
+	Bucket *s3.Bucket
+}
+
+func (s *S3HTTP) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	f, err := s.Bucket.GetReader(r.URL.Path)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	defer f.Close()
+
+	io.Copy(w, f)
 }
